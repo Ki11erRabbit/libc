d2ca2886b04cb94e089f5d301fca8c1d171f483f
ki11errabbit <ki11errabbit@posteo.net>
2023-06-13 22:12:16 -0600

    fcntl should now work

--- fcntl-inline.c
+++ fcntl-inline.c
@@ -20,7 +20,7 @@ static inline kk_integer_t kk_fcntl_wrapper_void(kk_integer_t fd, kk_integer_t c
     return kk_integer_from_small(fcntl(file_descriptor, val));
 }
 
-static inline kk_integer_t kk_fcntl_wrapper_arg(kk_integer fd, kk_integer_t cmd, kk_integer_t arg, kk_context_t* _ctx) {
+static inline kk_integer_t kk_fcntl_wrapper_arg(kk_integer_t fd, kk_integer_t cmd, kk_integer_t arg, kk_context_t* _ctx) {
     uint64_t file_descriptor = (fd.ibox - 1) / 4;
     uint64_t val = (cmd.ibox - 1) / 4;
     uint64_t arg_val = (arg.ibox - 1) / 4;
